2683f49bc66b043e0d144ac723cc43e2198bc8b9
AlexSpohr <alexspohr436@gmail.com>
2024-02-20 19:52:48 -0300

    feat: UserController created

--- src/main/java/com/compassuol/sp/challenge/msuser/web/controller/UserController.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.compassuol.sp.challenge.msuser.web.controller;
 
+
+import com.compassuol.sp.challenge.msuser.web.dto.UserCreateDto;
+import com.compassuol.sp.challenge.msuser.web.dto.UserResponseDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserCreateDto userCreateDto) {
+        return null;
+    }
 }
