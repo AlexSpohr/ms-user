829a950218aac7a8c929a81a221fc22b6aacd127
AlexSpohr <alexspohr436@gmail.com>
2024-02-20 19:51:13 -0300

    feat: userCreateDto and UserResponseDto created

---
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/dto/UserCreateDto.java
@@ -0,0 +1,42 @@
+package com.compassuol.sp.challenge.msuser.web.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.util.Date;
+
+public class UserCreateDto {
+
+    @NotBlank
+    @Size(min = 3)
+    private String fistName;
+
+    @NotBlank
+    @Size(min = 3)
+    private String lastName;
+
+    @NotBlank
+    @CPF
+    private String cpf;
+
+    @NotNull
+    @Past
+    private Date birthdate;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Pattern(regexp = "\\d{5}-\\d{3}")
+    private String cep;
+
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+
+    @NotNull
+    private Boolean active;
+
+}
