4b1c2dc13cc467f641be0d02be0d33ea2ea3a424
AlexSpohr <alexspohr436@gmail.com>
2024-02-20 21:11:34 -0300

    feat: development of the createUser method in the controller

--- src/main/java/com/compassuol/sp/challenge/msuser/web/controller/UserController.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.compassuol.sp.challenge.msuser.web.controller;
 
 
+import com.compassuol.sp.challenge.msuser.domain.user.entity.User;
+import com.compassuol.sp.challenge.msuser.domain.user.service.UserService;
 import com.compassuol.sp.challenge.msuser.web.dto.UserCreateDto;
 import com.compassuol.sp.challenge.msuser.web.dto.UserResponseDto;
+import com.compassuol.sp.challenge.msuser.web.dto.mapper.UserMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,11 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/v1/users")
 public class UserController {
+
+    private final UserService userService;
 
     @PostMapping
     public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserCreateDto userCreateDto) {
-        return null;
+        User user = userService.createUser(UserMapper.toUser(userCreateDto));
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(UserMapper.toDto(user));
     }
 }
