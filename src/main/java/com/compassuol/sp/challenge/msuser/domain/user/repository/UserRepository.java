0e8f871ffdb5d618edb24b580c5848983c7badf4
AlexSpohr <alexspohr436@gmail.com>
2024-02-20 21:12:22 -0300

    feat: UserRepository created

---
+++ src/main/java/com/compassuol/sp/challenge/msuser/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.compassuol.sp.challenge.msuser.domain.user.repository;
+
+import com.compassuol.sp.challenge.msuser.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
