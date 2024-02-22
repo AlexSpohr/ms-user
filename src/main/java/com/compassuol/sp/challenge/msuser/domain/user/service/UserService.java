afed0f72163bc36157be337a279ac609910f08e5
AlexSpohr <alexspohr436@gmail.com>
2024-02-22 15:01:20 -0300

    feat: PasswordEncoder in createUser

--- src/main/java/com/compassuol/sp/challenge/msuser/domain/user/service/UserService.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/domain/user/service/UserService.java
@@ -6,14 +6,17 @@ import com.compassuol.sp.challenge.msuser.domain.user.exception.UserUniqueViolat
 import com.compassuol.sp.challenge.msuser.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     public User createUser(User user) {
         try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
             return userRepository.save(user);
         } catch (DataIntegrityViolationException e) {
             throw new UserUniqueViolationException("CPF/Email already exists in the system.");
