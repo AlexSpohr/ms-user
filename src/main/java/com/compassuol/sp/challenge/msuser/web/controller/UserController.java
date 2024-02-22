7deb5a955fbf2a7261997b25f53833e8b8e4eb74
AlexSpohr <alexspohr436@gmail.com>
2024-02-22 15:53:42 -0300

    feat: getUserById created in UserController

--- src/main/java/com/compassuol/sp/challenge/msuser/web/controller/UserController.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/controller/UserController.java
@@ -8,13 +8,9 @@ import com.compassuol.sp.challenge.msuser.web.dto.UserResponseDto;
 import com.compassuol.sp.challenge.msuser.web.dto.mapper.UserMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -29,5 +25,11 @@ public class UserController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(UserMapper.toDto(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(UserMapper.toDto(user));
     }
 }
