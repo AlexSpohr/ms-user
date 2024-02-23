18d7f431df5b0e6f5a29f3f18427a11734a28640
AlexSpohr <alexspohr436@gmail.com>
2024-02-23 14:35:29 -0300

    feat: documentation login

--- src/main/java/com/compassuol/sp/challenge/msuser/web/controller/LoginController.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/controller/LoginController.java
@@ -3,7 +3,13 @@ package com.compassuol.sp.challenge.msuser.web.controller;
 import com.compassuol.sp.challenge.msuser.domain.user.jwt.JwtToken;
 import com.compassuol.sp.challenge.msuser.domain.user.service.AuthenticationService;
 import com.compassuol.sp.challenge.msuser.web.dto.LoginCreateDto;
+import com.compassuol.sp.challenge.msuser.web.dto.UserResponseDto;
 import com.compassuol.sp.challenge.msuser.web.dto.mapper.LoginMapper;
+import com.compassuol.sp.challenge.msuser.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +28,13 @@ public class LoginController {
 
     private final AuthenticationService authenticationService;
 
+    @Operation(summary = "Login", description = "Feature to login a user by email and password",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "User successfully logged in",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
+                    @ApiResponse(responseCode = "422", description = "Resource not processed due to invalid input data",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping
     public ResponseEntity<JwtToken> loginUser(@RequestBody @Valid LoginCreateDto loginCreateDto) {
         JwtToken token = authenticationService.authenticate(LoginMapper.toLogin(loginCreateDto));
