5d1ccd5b2063e39d055adfcd00a84f25e491916e
AlexSpohr <alexspohr436@gmail.com>
2024-02-23 14:22:10 -0300

    feat: documentation create user

--- src/main/java/com/compassuol/sp/challenge/msuser/web/controller/UserController.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/controller/UserController.java
@@ -8,6 +8,11 @@ import com.compassuol.sp.challenge.msuser.web.dto.UserCreateDto;
 import com.compassuol.sp.challenge.msuser.web.dto.UserPasswordDto;
 import com.compassuol.sp.challenge.msuser.web.dto.UserResponseDto;
 import com.compassuol.sp.challenge.msuser.web.dto.mapper.UserMapper;
+import com.compassuol.sp.challenge.msuser.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,6 +26,15 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "Create a new user", description = "Feature to create a new user",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
+                    @ApiResponse(responseCode = "409", description = "Usuário já cadastrado no sistema",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por dados de entrada invalidos",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping
     public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserCreateDto userCreateDto) {
         User user = userService.createUser(UserMapper.toEntity(userCreateDto));
