3798924ae66ebf6ccef16844ba32208ea31cc5a9
AlexSpohr <alexspohr436@gmail.com>
2024-02-28 18:09:50 -0300

    feat: producer getAddressById created

--- src/main/java/com/compassuol/sp/challenge/msuser/web/producer/UserProducerAddress.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/producer/UserProducerAddress.java
@@ -1,11 +1,17 @@
 package com.compassuol.sp.challenge.msuser.web.producer;
 
+import com.compassuol.sp.challenge.msuser.web.dto.AddressDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(value = "ms-address", url = "localhost:8081/v1/address")
 public interface UserProducerAddress {
     @PostMapping(value = "/{cep}")
-    void saveAddress(@PathVariable String cep);
+    AddressDto saveAddress(@PathVariable String cep);
+
+    @GetMapping("/{id}")
+    AddressDto getAddressById(@PathVariable("id") Long id);
+
 }
