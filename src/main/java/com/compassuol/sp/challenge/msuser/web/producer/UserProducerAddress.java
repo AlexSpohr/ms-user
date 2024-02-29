399da8b1d6d4342a1af7c9f1da1fbb3740f3d55b
AlexSpohr <alexspohr436@gmail.com>
2024-02-28 22:52:24 -0300

    feat: adding jwt parameters to save and search address in producer

--- src/main/java/com/compassuol/sp/challenge/msuser/web/producer/UserProducerAddress.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/producer/UserProducerAddress.java
@@ -5,13 +5,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(value = "ms-address", url = "localhost:8081/v1/address")
 public interface UserProducerAddress {
     @PostMapping(value = "/{cep}")
-    AddressDto saveAddress(@PathVariable String cep);
+    AddressDto saveAddress(@PathVariable String cep, @RequestHeader("Authorization") String token);
 
     @GetMapping("/{id}")
-    AddressDto getAddressById(@PathVariable("id") Long id);
+    AddressDto getAddressById(@PathVariable("id") Long id, @RequestHeader("Authorization") String token);
 
 }
