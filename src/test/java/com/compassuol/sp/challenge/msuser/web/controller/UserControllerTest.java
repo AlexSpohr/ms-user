4f9d8293beb10fbd30d2b262b9bc02b55dd4d56f
AlexSpohr <alexspohr436@gmail.com>
2024-03-01 00:12:37 -0300

    feat: getUserById tests in UserControllerTest

--- src/test/java/com/compassuol/sp/challenge/msuser/web/controller/UserControllerTest.java
+++ src/test/java/com/compassuol/sp/challenge/msuser/web/controller/UserControllerTest.java
@@ -105,6 +105,49 @@ class UserControllerTest {
                 .andExpect(status().isUnprocessableEntity());
     }
 
+    @Test
+    void getUserById_WithExistingId_ReturnsOk200() throws Exception {
+        long id = 1L;
+        objectMapper.setDateFormat(new SimpleDateFormat("dd/MM/yyyy"));
+
+        when(userService.getUserAddressById(any())).thenReturn(USER_RESPONSE);
+
+        mockMvc
+                .perform(
+                        get("/v1/users/" + id)
+                                .header("Authorization", "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0ZTJAZW1haWwuY29tIiwiaWF0IjoxNzA5MjMxOTY0LCJleHAiOjE3MDkyMzI1NjR9.VxjeDLGyZsFQle5yIml-bmYg-23JmOtvL33QbDCiE98")
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getUserById_WithNonExistingId_ReturnsNotFound404() throws Exception {
+        long nonExistingId = -1L;
+
+        when(userService.getUserAddressById(any())).thenThrow(new EntityNotFoundException("User not found."));
+
+        mockMvc
+                .perform(
+                        get("/v1/users/" + nonExistingId)
+                                .header("Authorization", "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0ZTJAZW1haWwuY29tIiwiaWF0IjoxNzA5MjMxOTY0LCJleHAiOjE3MDkyMzI1NjR9.VxjeDLGyZsFQle5yIml-bmYg-23JmOtvL33QbDCiE98")
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getUserById_WithNotAuthorizedId_ReturnsForbidden403() throws Exception {
+        long unauthorizedId = 1L;
+
+        when(userService.getUserAddressById(any())).thenThrow(new AccessDeniedException("User is not authorized to access this resource."));
+
+        mockMvc
+                .perform(
+                        get("/v1/users/" + unauthorizedId)
+                                .header("Authorization", "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0ZTJAZW1haWwuY29tIiwiaWF0IjoxNzA5MjMxOTY0LCJleHAiOjE3MDkyMzI1NjR9.VxjeDLGyZsFQle5yIml-bmYg-23JmOtvL33QbDCiE98")
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+
 
 
 }
